6724d1f0cdbc721c789076d9f950c31dd6b19be7
Nattapat Sittichai <73817577+Voidntpx@users.noreply.github.com>
2023-05-05 18:05:44 +0700

    add logger go fiber

--- Backend/routes/user_route.go
+++ Backend/routes/user_route.go
@@ -4,11 +4,15 @@ import (
 	"fiber-mongo-api/controllers"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 
 	jwtware "github.com/gofiber/jwt/v3"
 )
 
 func UserRoute(app *fiber.App) {
+	app.Use(logger.New(logger.Config{
+		TimeZone: "Asia/Bangkok",
+	}))
 	app.Post("/user/login", controllers.Login)
 
 	app.Use(jwtware.New(jwtware.Config{SigningKey: []byte("ultima")}))
