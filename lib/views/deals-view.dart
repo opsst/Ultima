837ea37ae5d17dd2a249ae2db960d0008561292c
Putita Techapat <putita.techapat@gmail.com>
2023-05-28 16:31:48 +0700

    :-)

---
+++ lib/views/deals-view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class AllDealsView extends StatefulWidget {
+  const AllDealsView({Key? key}) : super(key: key);
+
+  @override
+  State<AllDealsView> createState() => _AllDealsViewState();
+}
+
+class _AllDealsViewState extends State<AllDealsView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
